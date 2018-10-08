4c8ca5aa45d28f169c424226a0cabca25aa994a9
annao421 <anna506921@gmail.com>
2018-10-08 14:40:02 +0900

    93페이지 주어진 배열에서 가장 많이 등장하는 숫자 반환

---
+++ 93p.cpp
@@ -0,0 +1,20 @@
+// 수행시간 N^2
+ 
+int majority1(vector<int>& A) {
+    int N = A.size();
+    int majority = -1, majorityCount = 0;
+    for (int i=0; i<N; i++){
+        int V=A[i], count = 0;
+        for(int j=0;j<N;j++){
+            if(A[j]==V) ++count;
+        }
+    if(count>majorityCount) {
+        majorityCount=count;
+        majority=V;
+        }
+    }
+ //cout<<majority;
+ return majority;
+}
+
+
